b0b3949c5c10336471fb2043541381cab4139523
Dmitry <kostenko.market@gmail.com>
2015-08-23 12:31:04 +0300

    apply gofmt

--- tgbot_api.go
+++ tgbot_api.go
@@ -2,230 +2,247 @@
 package tgbot
 
 import (
-    "sync"
-    "log"
-    "net/http"
-    // "net/url"
-    "io/ioutil"
-    "strconv"
-    "mime/multipart"
-    "bytes"
-    "path/filepath"
-    "io"
-    // "reflect"
+	"log"
+	"net/http"
+	"sync"
+	// "net/url"
+	"bytes"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"path/filepath"
+	"strconv"
+	// "reflect"
 )
 
 type (
-    Param struct {
-        Key string
-        Value string
-        FileUrl string
-    }
+	Param struct {
+		Key     string
+		Value   string
+		FileUrl string
+	}
 
-    ApiManager struct {
-        baseApi string
-        secretToken string
-        DebugMode bool
-    }
+	ApiManager struct {
+		baseApi     string
+		secretToken string
+		DebugMode   bool
+	}
 )
 
 var (
-    once sync.Once
-    instance *ApiManager
+	once     sync.Once
+	instance *ApiManager
 )
 
 // NewApiManager returns a singleton API manager.
 func NewApiManager(baseApi string, secretToken string, debugMode bool) *ApiManager {
-    once.Do(func() {
-        instance = &ApiManager{
-            baseApi: baseApi,
-            secretToken: secretToken,
-            DebugMode: debugMode,
-        }
-    })
-    return instance
+	once.Do(func() {
+		instance = &ApiManager{
+			baseApi:     baseApi,
+			secretToken: secretToken,
+			DebugMode:   debugMode,
+		}
+	})
+	return instance
 }
 
 func (this *ApiManager) SendMessage(chatId int, text string, replyToMessageId int, replyMarkup string) {
-    url := this.getUrl("sendMessage")
+	url := this.getUrl("sendMessage")
 
-    params := []*Param{
-        &Param{Key: "text", Value: text},
-        &Param{Key: "chat_id", Value: strconv.Itoa(chatId)},
-        &Param{Key: "reply_markup", Value: replyMarkup},
-        // &Param{Key: "reply_markup", Value: "{\"force_reply\":true}"},
-    }
-    
-    if replyToMessageId > 0 {
-        params = append(params, &Param{Key: "reply_to_message_id", Value: strconv.Itoa(replyToMessageId)})
-    }
+	params := []*Param{
+		&Param{Key: "text", Value: text},
+		&Param{Key: "chat_id", Value: strconv.Itoa(chatId)},
+		&Param{Key: "reply_markup", Value: replyMarkup},
+		// &Param{Key: "reply_markup", Value: "{\"force_reply\":true}"},
+	}
 
-    _, err := this.sendPost(url, params)
-    if err != nil {
-        log.Println("ERROR", err)
-    }
+	if replyToMessageId > 0 {
+		params = append(params, &Param{Key: "reply_to_message_id", Value: strconv.Itoa(replyToMessageId)})
+	}
+
+	_, err := this.sendPost(url, params)
+	if err != nil {
+		log.Println("ERROR", err)
+	}
 }
 
 func (this *ApiManager) GetMe() {
-    url := this.getUrl("getMe")
+	url := this.getUrl("getMe")
 
-    this.sendGet(url, nil)
+	this.sendGet(url, nil)
 }
 
 func (this *ApiManager) SendPhoto(chatId int, fileUrl string, caption string, replyToMessageId int) {
-    url := this.getUrl("sendPhoto")
+	url := this.getUrl("sendPhoto")
 
-    params := []*Param{
-        &Param{Key: "photo", FileUrl: fileUrl},
-        &Param{Key: "chat_id", Value: strconv.Itoa(chatId)},
-    }
-    
-    if caption != "" {
-        params = append(params, &Param{Key: "caption", Value: caption},)
-    }
-    
-    if replyToMessageId > 0 {
-        params = append(params, &Param{Key: "reply_to_message_id", Value: strconv.Itoa(replyToMessageId)})
-    }
+	params := []*Param{
+		&Param{Key: "photo", FileUrl: fileUrl},
+		&Param{Key: "chat_id", Value: strconv.Itoa(chatId)},
+	}
 
-    this.sendPost(url, params)
+	if caption != "" {
+		params = append(params, &Param{Key: "caption", Value: caption})
+	}
+
+	if replyToMessageId > 0 {
+		params = append(params, &Param{Key: "reply_to_message_id", Value: strconv.Itoa(replyToMessageId)})
+	}
+
+	this.sendPost(url, params)
 }
 
 func (this *ApiManager) SetWebhook(webhookUrl string) {
-    url := this.getUrl("setWebhook")
-    
-    params := []*Param{
-        &Param{Key: "url", Value: webhookUrl},
-    }
-    
-    this.sendGet(url, params)
+	url := this.getUrl("setWebhook")
+
+	params := []*Param{
+		&Param{Key: "url", Value: webhookUrl},
+	}
+
+	this.sendGet(url, params)
 }
 
 func (this *ApiManager) sendPost(url string, params []*Param) ([]int, error) {
-    body := &bytes.Buffer{}
-    writer := multipart.NewWriter(body)
-    
-    for _, param := range params {
-        if param.FileUrl != "" {
-            res, err := http.Get(param.FileUrl)
-            
-            if err != nil { return nil, err }
-            defer res.Body.Close()
-            
-            file := res.Body
-            part, err := writer.CreateFormFile(param.Key, filepath.Base(param.FileUrl))
-            if err != nil { return nil, err }
-            
-            _, err = io.Copy(part, file)
-            if err != nil { return nil, err }
-        } else {
-            writer.WriteField(param.Key, param.Value)
-        }
-    }
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
 
-    err := writer.Close()
-    if err != nil { return nil, err }
+	for _, param := range params {
+		if param.FileUrl != "" {
+			res, err := http.Get(param.FileUrl)
 
-    req, _ := http.NewRequest("POST", url, body)
-    req.Header.Set("Content-Type", writer.FormDataContentType())
-    
-    client := &http.Client{}
-    resp, err := client.Do(req)
-    
-    if err != nil { return nil, err }
-    defer resp.Body.Close()
+			if err != nil {
+				return nil, err
+			}
+			defer res.Body.Close()
 
-    resBody, _ := ioutil.ReadAll(resp.Body)
+			file := res.Body
+			part, err := writer.CreateFormFile(param.Key, filepath.Base(param.FileUrl))
+			if err != nil {
+				return nil, err
+			}
 
-    if this.DebugMode {
-        log.Println("response Status:", resp.Status)
-        log.Println("response Headers:", resp.Header)
-        log.Println("response Body:", string(resBody))
-    }
-    
-    return nil, nil
+			_, err = io.Copy(part, file)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			writer.WriteField(param.Key, param.Value)
+		}
+	}
+
+	err := writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	req, _ := http.NewRequest("POST", url, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	resBody, _ := ioutil.ReadAll(resp.Body)
+
+	if this.DebugMode {
+		log.Println("response Status:", resp.Status)
+		log.Println("response Headers:", resp.Header)
+		log.Println("response Body:", string(resBody))
+	}
+
+	return nil, nil
 }
 
 func (this *ApiManager) sendGet(url string, params []*Param) {
-    url += "?"
-    
-    for i, param := range params {
-        if i > 0 { url += "&" }
-        url += param.Key + "=" + param.Value
-    }
-    
-    resp, err := http.Get(url)
-    
-    if err != nil {
-    	log.Println(err)
-    }
-    
-    defer resp.Body.Close()
-    
-    body, _ := ioutil.ReadAll(resp.Body)
-    
-    if this.DebugMode {
-        log.Println("response Status:", resp.Status)
-        log.Println("response Headers:", resp.Header)
-        log.Println("response Body:", string(body))
-    }
+	url += "?"
+
+	for i, param := range params {
+		if i > 0 {
+			url += "&"
+		}
+		url += param.Key + "=" + param.Value
+	}
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	if this.DebugMode {
+		log.Println("response Status:", resp.Status)
+		log.Println("response Headers:", resp.Header)
+		log.Println("response Body:", string(body))
+	}
 }
 
 func (this *ApiManager) getUrl(method string) (url string) {
-    url = this.baseApi + "/" + this.secretToken + "/" + method
-    return
+	url = this.baseApi + "/" + this.secretToken + "/" + method
+	return
 }
-
-
-
-
 
 func sendPost(url string, params []*Param) (string, error) {
-    body := &bytes.Buffer{}
-    writer := multipart.NewWriter(body)
-    
-    for _, param := range params {
-        if param.FileUrl != "" {
-            res, err := http.Get(param.FileUrl)
-            
-            if err != nil { return "", err }
-            defer res.Body.Close()
-            
-            file := res.Body
-            part, err := writer.CreateFormFile(param.Key, filepath.Base(param.FileUrl))
-            if err != nil { return "", err }
-            
-            _, err = io.Copy(part, file)
-            if err != nil { return "", err }
-        } else {
-            writer.WriteField(param.Key, param.Value)
-        }
-    }
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
 
-    err := writer.Close()
-    if err != nil { return "", err }
+	for _, param := range params {
+		if param.FileUrl != "" {
+			res, err := http.Get(param.FileUrl)
 
-    req, _ := http.NewRequest("POST", url, body)
-    req.Header.Set("Content-Type", writer.FormDataContentType())
-    
-    client := &http.Client{}
-    resp, err := client.Do(req)
-    
-    if err != nil { return "", err }
-    defer resp.Body.Close()
+			if err != nil {
+				return "", err
+			}
+			defer res.Body.Close()
 
-    resBody, _ := ioutil.ReadAll(resp.Body)
-    json := string(resBody)
+			file := res.Body
+			part, err := writer.CreateFormFile(param.Key, filepath.Base(param.FileUrl))
+			if err != nil {
+				return "", err
+			}
 
-    log.Println("response Status:", resp.Status)
-    log.Println("response Headers:", resp.Header)
-    log.Println("response Body:", json)
-    
-    return json, nil
+			_, err = io.Copy(part, file)
+			if err != nil {
+				return "", err
+			}
+		} else {
+			writer.WriteField(param.Key, param.Value)
+		}
+	}
+
+	err := writer.Close()
+	if err != nil {
+		return "", err
+	}
+
+	req, _ := http.NewRequest("POST", url, body)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	resBody, _ := ioutil.ReadAll(resp.Body)
+	json := string(resBody)
+
+	log.Println("response Status:", resp.Status)
+	log.Println("response Headers:", resp.Header)
+	log.Println("response Body:", json)
+
+	return json, nil
 }
 
-
 func getUrl(method string) (url string) {
-    url = "https://api.telegram.org/bot94859224:AAFiKYi7ZgU8CDOyH6HJ-42ON4EThadtSy0/" + method
-    return
+	url = "https://api.telegram.org/bot94859224:AAFiKYi7ZgU8CDOyH6HJ-42ON4EThadtSy0/" + method
+	return
 }
--- tgbot_send_message.go
+++ tgbot_send_message.go
@@ -2,59 +2,63 @@
 package tgbot
 
 import (
-    // "log"
-    "strconv"
-    "io"
-    "strings"
-    "encoding/json"
+	// "log"
+	"encoding/json"
+	"io"
+	"strconv"
+	"strings"
 )
 
 type SendMessageRequest struct {
-    ChatId int
-    Text string
-    ReplyToMessageId int
-    ReplyMarkup
+	ChatId           int
+	Text             string
+	ReplyToMessageId int
+	ReplyMarkup
 }
 
 type ReplyMarkup struct {
-    Keyboard [][]string `json:"keyboard"`
-    ResizeKeyboard bool `json:"resize_keyboard"`
-    OneTimeKeyboard bool `json:"one_time_keyboard"`
-    Selective bool `json:"selective"`
-    HideKeyboard bool `json:"hide_keyboard"`
-    ForceReply bool `json:"force_reply"`
+	Keyboard        [][]string `json:"keyboard"`
+	ResizeKeyboard  bool       `json:"resize_keyboard"`
+	OneTimeKeyboard bool       `json:"one_time_keyboard"`
+	Selective       bool       `json:"selective"`
+	HideKeyboard    bool       `json:"hide_keyboard"`
+	ForceReply      bool       `json:"force_reply"`
 }
 
-func (this *SendMessageRequest) Send() (*SendMessageResponse, error){
-    url := getUrl("sendMessage")
-    
-    params, err := this.getParams()
-    if err != nil { return nil, err }
-    
-    respJson, err := sendPost(url, params)
-    if err != nil { return nil, err}
-    
-    dec := json.NewDecoder(strings.NewReader(respJson))
-    
-    var resp *SendMessageResponse
+func (this *SendMessageRequest) Send() (*SendMessageResponse, error) {
+	url := getUrl("sendMessage")
+
+	params, err := this.getParams()
+	if err != nil {
+		return nil, err
+	}
+
+	respJson, err := sendPost(url, params)
+	if err != nil {
+		return nil, err
+	}
+
+	dec := json.NewDecoder(strings.NewReader(respJson))
+
+	var resp *SendMessageResponse
 	if err := dec.Decode(&resp); err == io.EOF {
 		return resp, nil
 	} else if err != nil {
-        return nil, err
+		return nil, err
 	}
-    
-    return resp, nil
+
+	return resp, nil
 }
 
-func (this *SendMessageRequest) getParams() ([]*Param, error){
-    replyMarkupJson, _ := json.Marshal(this.ReplyMarkup)
-    
-    res := []*Param{
-        &Param{Key: "chat_id", Value: strconv.Itoa(this.ChatId)}, 
-        &Param{Key: "text", Value: this.Text},
-        &Param{Key: "reply_markup", Value: string(replyMarkupJson)},
-        &Param{Key: "reply_to_message_id", Value: string(this.ReplyToMessageId)},
-    }
-    
-    return res, nil
+func (this *SendMessageRequest) getParams() ([]*Param, error) {
+	replyMarkupJson, _ := json.Marshal(this.ReplyMarkup)
+
+	res := []*Param{
+		&Param{Key: "chat_id", Value: strconv.Itoa(this.ChatId)},
+		&Param{Key: "text", Value: this.Text},
+		&Param{Key: "reply_markup", Value: string(replyMarkupJson)},
+		&Param{Key: "reply_to_message_id", Value: string(this.ReplyToMessageId)},
+	}
+
+	return res, nil
 }
